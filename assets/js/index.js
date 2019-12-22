d369de2e3fd6d12e4b3b2650b63eaaee8d7b1430
prashanthumbe-labs <prashant.humbe@infinitylabs.in>
2019-12-22 23:29:10 +0530

    Initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,12 @@ var Typer={
 	speed:2,
 	file:"", 
 	accessCount:0,
-	deniedCount:0, 
+	deniedCount:0,
+
 	init: function(){
-		accessCountimer=setInterval(function(){Typer.updLstChr();},500); 
+		accessCountimer = setInterval(function(){Typer.updLstChr();},500); 
 		$.get(Typer.file,function(data){
-			Typer.text=data;
+			Typer.text = data;
 			Typer.text = Typer.text.slice(0, Typer.text.length-1);
 		});
 	},
@@ -17,82 +18,74 @@ var Typer={
 	content:function(){
 		return $("#console").html();
 	},
- 
+
 	write:function(str){
 		$("#console").append(str);
 		return false;
 	},
  
-	addText:function(key){
-		
+	addText:function(key){		
 		if(key.keyCode==18){
-			Typer.accessCount++; 
-			
+			Typer.accessCount++;
 			if(Typer.accessCount>=3){
 				Typer.makeAccess(); 
 			}
-		}
-		
-    		else if(key.keyCode==20){
-			Typer.deniedCount++; 
-			
+		}		
+    	else if(key.keyCode==20){
+			Typer.deniedCount++;
 			if(Typer.deniedCount>=3){
 				Typer.makeDenied(); 
 			}
-		}
-		
-    		else if(key.keyCode==27){ 
+		}		
+    	else if(key.keyCode==27){ 
 			Typer.hidepop(); 
-		}
-		
-    		else if(Typer.text){ 
+		}		
+    	else if(Typer.text){ 
 			var cont=Typer.content(); 
-			if(cont.substring(cont.length-1,cont.length)=="|") 
+			if(cont.substring(cont.length-1,cont.length)=="|"){
 				$("#console").html($("#console").html().substring(0,cont.length-1)); 
+			}
 			if(key.keyCode!=8){ 
 				Typer.index+=Typer.speed;	
 			}
       		else {
-			if(Typer.index>0) 
-				Typer.index-=Typer.speed;
+				if(Typer.index>0){
+
+					Typer.index-=Typer.speed;
+				} 
 			}
 			var text=Typer.text.substring(0,Typer.index)
-			var rtn= new RegExp("\n", "g"); 
-	
+			var rtn= new RegExp("\n", "g");
 			$("#console").html(text.replace(rtn,"<br/>"));
 			window.scrollBy(0,50); 
-		}
-		
+		}		
 		if (key.preventDefault && key.keyCode != 122) { 
 			key.preventDefault()
-		};  
-		
+		};
 		if(key.keyCode != 122){ // otherway prevent keys default behavior
 			key.returnValue = false;
 		}
 	},
- 
+
 	updLstChr:function(){ 
 		var cont=this.content();
-		if(cont.substring(cont.length-1,cont.length)=="|") 
+		if(cont.substring(cont.length-1,cont.length)=="|"){
 			$("#console").html($("#console").html().substring(0,cont.length-1)); 
-		
-		else
+		}
+		else{
 			this.write("|"); // else write it
+		}
 	}
 }
  
 function replaceUrls(text) {
 	var http = text.indexOf("http://");
 	var space = text.indexOf(".me ", http);
-	
 	if (space != -1) { 
 		var url = text.slice(http, space-1);
 		return text.replace(url, "<a href=\""  + url + "\">" + url + "</a>");
-	} 
-	
-	else {
-		return text
+	} else {
+		return text;
 	}
 }
 
@@ -100,12 +93,10 @@ Typer.speed=3;
 Typer.file="prashanthumbe.txt";
 Typer.init();
  
-var timer = setInterval("t();", 30);
+var timer = setInterval("t();", 50);
 function t() {
-	Typer.addText({"keyCode": 123748});
-	
+	Typer.addText({"keyCode": 123748});	
 	if (Typer.index > Typer.text.length) {
 		clearInterval(timer);
 	}
-}
- 
+} 
